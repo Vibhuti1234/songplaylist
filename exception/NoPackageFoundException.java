d1e58fdc7efef85a993fde5c28575bdaf340485c
Vibhuti1234 <51270852+Vibhuti1234@users.noreply.github.com>
2019-12-24 09:07:30 +0530

    Add files via upload

---
+++ exception/NoPackageFoundException.java
@@ -0,0 +1,36 @@
+package com.mindtree.travelmanagementsystemapplication.exception;
+
+public class NoPackageFoundException extends TravelManagementControllerException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoPackageFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoPackageFoundException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoPackageFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoPackageFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoPackageFoundException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
